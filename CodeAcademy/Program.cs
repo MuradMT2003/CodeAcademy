be06537e9f42eb0b8c2f6d6c3961edc359722c44
Murad Mammadzade <mammadzade03@gmail.com>
2023-04-25 08:33:43 +0400

    Add project files.

---
+++ CodeAcademy/Program.cs
@@ -0,0 +1,4 @@
+﻿using CodeAcademy;
+var stu1 = new Student("Murad","Mammadzada","P-137",90.6,true);
+Console.WriteLine($"{stu1.FullName()}\n{stu1.Details()}\n{stu1.PermissionForNextExam()}");
+
---
+++ CodeAcademy/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAcademy
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public double Point { get; set; }
+        public bool isGraduated { get; set; }
+
+        public Student(string name, string surname,string group,double point,bool isGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            this.isGraduated = isGraduated;
+        }
+        
+        public  string FullName()
+        {
+            return $"{Name} {Surname}";
+        }
+        public string Details()
+        {
+            return $"{FullName()} {Group} {Point} {(isGraduated?"Mezun olub":"Mezun olmayib")}";
+        }
+        public string PermissionForNextExam()
+        {
+            return Point>80?"Imtahana gire bilersen":"Imtahana gire bilmezsen";
+        }
+    }
+}
