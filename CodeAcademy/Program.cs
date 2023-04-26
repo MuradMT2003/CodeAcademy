0b1998370bbc170f507c2cd66993a13ef935e389
Murad Mammadzade <mammadzade03@gmail.com>
2023-04-26 08:55:47 +0400

    done encapsulation project

--- CodeAcademy/Program.cs
+++ CodeAcademy/Program.cs
@@ -1,4 +1,11 @@
 ﻿using CodeAcademy;
-var stu1 = new Student("Murad","Mammadzada","P-137",90.6,true);
-Console.WriteLine($"{stu1.FullName()}\n{stu1.Details()}\n{stu1.PermissionForNextExam()}");
+
+Student s=new Student()
+{
+    Name="Murad",
+    Surname="Mammadzada"
+};
+Group group = new Group(1,5);
+group.AddStudent(s);
+group.GetStudents();
 
--- CodeAcademy/Student.cs
+++ CodeAcademy/Student.cs
@@ -10,30 +10,6 @@ namespace CodeAcademy
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Group { get; set; }
-        public double Point { get; set; }
-        public bool isGraduated { get; set; }
-
-        public Student(string name, string surname,string group,double point,bool isGraduated)
-        {
-            Name = name;
-            Surname = surname;
-            Group = group;
-            Point = point;
-            this.isGraduated = isGraduated;
-        }
-        
-        public  string FullName()
-        {
-            return $"{Name} {Surname}";
-        }
-        public string Details()
-        {
-            return $"{FullName()} {Group} {Point} {(isGraduated?"Mezun olub":"Mezun olmayib")}";
-        }
-        public string PermissionForNextExam()
-        {
-            return Point>80?"Imtahana gire bilersen":"Imtahana gire bilmezsen";
-        }
+       
     }
 }
